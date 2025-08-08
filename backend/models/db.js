8c5770becedab457530a6d33322c67b345992251
dawit-yit <dawityitbarek18@gmail.com>
2025-08-08 11:25:31 +0300

    Modify the configuation of database

--- backend/models/db.js
+++ backend/models/db.js
@@ -5,11 +5,11 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 export default pool;
